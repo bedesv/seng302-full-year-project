f1b2be573dbe9afbc625404eecf855190484195c
Sam <smc397@uclive.ac.nz>
2022-09-25 13:56:51 +1300

    #feature #refactor pair[bjs185] fixed issue with background appearing white. Added user statistics fragment. Started adding skill and cat pages as fragments. Had issues with getting the thymeleaf and JS to call the function correct. Bede zoomed to help me and it now works.

--- portfolio/src/main/resources/static/scripts/user/portfolio.js
+++ portfolio/src/main/resources/static/scripts/user/portfolio.js
@@ -18,3 +18,21 @@ async function updateUserPortfolio() {
     const portfolioWrapper = document.getElementById("portfolio_container")
     portfolioWrapper.innerHTML = portfolio
 }
+
+async function updateUserPortfolioWithSkill(selectedSkill) {
+    // Build the url
+    let url
+    url = new URL (`${CONTEXT}/portfolio-${USER_ID}-skill?`)
+
+    // Send a get request to fetch the user portfolio
+    // Receives the updated element HTML content as a response
+    const portfolio = await fetch(url+new URLSearchParams({skill: selectedSkill}), {
+        method: "GET"
+    }).then(res => {
+        return res.text()
+    })
+
+    // Update the page with the new HTML content
+    const portfolioWrapper = document.getElementById("portfolio_container")
+    portfolioWrapper.innerHTML = portfolio
+}
