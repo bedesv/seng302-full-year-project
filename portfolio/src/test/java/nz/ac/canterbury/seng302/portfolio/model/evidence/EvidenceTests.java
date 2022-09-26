e1ed0ac52d2e62ee66d66190fd15e6c8b1f52d0a
Sam <smc397@uclive.ac.nz>
2022-09-26 22:40:48 +1300

    #test Added tests for adding high-fives to evidence

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/evidence/EvidenceTests.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/evidence/EvidenceTests.java
@@ -1,10 +1,15 @@
 package nz.ac.canterbury.seng302.portfolio.model.evidence;
 
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.portfolio.model.user.User;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 class EvidenceTests {
     private Evidence testEvidence;
+
+    private User user;
 
     @BeforeEach
     void resetTestEvidence() {
@@ -57,4 +64,26 @@ class EvidenceTests {
         assertEquals("Commit index is invalid because it is less than one. Commit not deleted.", exception.getMessage());
         assertEquals(1, testEvidence.getNumberCommits());
     }
+
+    @Test
+    void whenUserHighFives_testAddHighFive() {
+        evidence.addHighFive(1, "Thomas Brown");
+        assertEquals(1, evidence.getHighFives);
+    }
+
+    @Test
+    void whenUserHasAlreadyHighFived_testAddHighFive() {
+        evidence.addHighFive(1, "Thomas Brown");
+        evidence.addHighFive(1, "Thomas Brown");
+        assertEquals(1, evidence.getHighFives);
+    }
+
+    @Test
+    void whenUserHasAlreadyHighFived_testRemoveHighFive() {
+        evidence.addHighFive(1, "Thomas Brown");
+        evidence.removeHighFive(1);
+        assertEquals(0, evidence.getHighFives);
+    }
+
+
 }
