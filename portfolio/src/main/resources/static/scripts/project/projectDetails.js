5903d25f4f83c796557bc9e8391fd95ad5272c1e
Billy Sandri <bsa57@uclive.ac.nz>
2022-09-30 12:14:11 +1300

    #Feature make project tabs highlight on active tab.

--- portfolio/src/main/resources/static/scripts/project/projectDetails.js
+++ portfolio/src/main/resources/static/scripts/project/projectDetails.js
@@ -83,7 +83,7 @@ async function updatePage() {
 
 function makeActive(el) {
     el.classList.add("active-carousel-tab");
-    let els = document.getElementsByClassName("group-tab__button");
+    let els = document.getElementsByClassName("projects__tab-button");
     for (let i = 0; i < els.length; i++) {
         if (els[i] !== el) {
             els[i].classList.remove("active-carousel-tab");
