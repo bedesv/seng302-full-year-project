6c1493e4bd771d4738450fb39845b8c5d9b57085
Sam McMillan <smc397@uclive.ac.nz>
2022-09-29 17:23:45 +1300

    #feature Removed console log statement

--- portfolio/src/main/resources/static/scripts/evidence/evidence.js
+++ portfolio/src/main/resources/static/scripts/evidence/evidence.js
@@ -3,7 +3,6 @@
  * @param evidenceId the evidence fragment that the high-five icon belongs too.
  */
 async function toggleEvidence(evidenceId) {
-    console.log(evidenceId);
     let url;
     url = new URL (`${CONTEXT}/evidence-${evidenceId}-highfive`);
 
