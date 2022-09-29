8f8044deeaafcbca7eb3a3ecada3cb0fe4562664
Danish Jahangir <dkj23@uclive.ac.nz>
2022-09-30 02:52:39 +1300

    #refactor Refactored the tab buttons on the project page to make them more consistent.

--- portfolio/src/main/resources/static/scripts/project/projectDetails.js
+++ portfolio/src/main/resources/static/scripts/project/projectDetails.js
@@ -80,3 +80,13 @@ async function updatePage() {
     // Update the page with the new data
     document.body.innerHTML = newPage.innerHTML
 }
+
+function makeActive(el) {
+    el.classList.add("active-carousel-tab");
+    let els = document.getElementsByClassName("group-tab__button");
+    for (let i = 0; i < els.length; i++) {
+        if (els[i] !== el) {
+            els[i].classList.remove("active-carousel-tab");
+        }
+    }
+}
